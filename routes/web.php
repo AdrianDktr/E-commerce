0cf182b57074798403099cfe79f43a2fac30015d
AdrianDktr <100672462+AdrianDktr@users.noreply.github.com>
2024-01-03 21:05:33 +0700

    show cart blade dan routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 
 /*
@@ -16,14 +18,28 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('auth.login');
+});
 
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
+//product
 Route::get('/index-product', [ProductController::class, 'index'])->name('index_product');
 Route::get('/create-product', [ProductController::class, 'create'])->name('create_product');
 Route::post('/store-product', [ProductController::class, 'store'])->name('store_product');
+Route::get('/show-product/{product}', [ProductController::class, 'show'])->name('show_product');
+Route::get('/edit-product/{product}', [ProductController::class, 'edit'])->name('edit_product');
+Route::patch('/update-product/{product}', [ProductController::class, 'update'])->name('update_product');
+Route::delete('/delete-product/{product}', [ProductController::class, 'delete'])->name('delete_product');
+
+//cart
+Route::post('/store-cart/{product}', [CartController::class, 'add_to_cart'])->name('store_cart');
+Route::get('/show-cart', [CartController::class, 'show'])->name('show_cart');
+Route::patch('/update-cart/{cart}', [CartController::class, 'update'])->name('update_cart');
+Route::delete('/delete-cart/{cart}', [CartController::class,'delete'])->name('delete_cart');
+
+//orders
+Route::post('/checkout-order',[OrderController::class, 'checkout'])->name('checkout');
