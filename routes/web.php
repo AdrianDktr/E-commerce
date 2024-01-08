292f50bc94b98ce9a4267ae8fee0cf9533b899ca
AdrianDktr <100672462+AdrianDktr@users.noreply.github.com>
2024-01-08 17:53:19 +0700

    penambahan middleware dan perubahan main acces

--- routes/web.php
+++ routes/web.php
@@ -20,37 +20,47 @@ use App\Http\Controllers\ProfileController;
 */
 
 Route::get('/', function () {
-    return view('auth.login');
+    return redirect()->route('index_product');
 });
 
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 //product
 Route::get('/index-product', [ProductController::class, 'index'])->name('index_product');
-Route::get('/create-product', [ProductController::class, 'create'])->name('create_product');
-Route::post('/store-product', [ProductController::class, 'store'])->name('store_product');
-Route::get('/show-product/{product}', [ProductController::class, 'show'])->name('show_product');
-Route::get('/edit-product/{product}', [ProductController::class, 'edit'])->name('edit_product');
-Route::patch('/update-product/{product}', [ProductController::class, 'update'])->name('update_product');
-Route::delete('/delete-product/{product}', [ProductController::class, 'delete'])->name('delete_product');
 
-//cart
-Route::post('/store-cart/{product}', [CartController::class, 'add_to_cart'])->name('store_cart');
-Route::get('/show-cart', [CartController::class, 'show'])->name('show_cart');
-Route::patch('/update-cart/{cart}', [CartController::class, 'update'])->name('update_cart');
-Route::delete('/delete-cart/{cart}', [CartController::class,'delete'])->name('delete_cart');
 
-//orders
-Route::post('/checkout-order',[OrderController::class, 'checkout'])->name('checkout');
-Route::get('/index-order',[OrderController::class,'index'])->name('index_order');
-Route::get('/show-order/{order}',[OrderController::class, 'show_detail_order'])->name('show_detail_order');
-Route::post('/order-payment/{order}',[OrderController::class, 'receipt'])->name('submit_payment_receipt');
-Route::post('/order-confirm-payment/{order}',[OrderController::class, 'confirm_payment'])->name('confirm_payment');
+Route::middleware(['admin'])->group(function(){
 
-//user information
-Route::get('/profile',[ProfileController::class,'show_profile'])->name('show_profile');
-Route::post('/edit-profile',[ProfileController::class, 'edit_profile'])->name('edit_profile');
+    Route::get('/create-product', [ProductController::class, 'create'])->name('create_product');
+    Route::post('/store-product', [ProductController::class, 'store'])->name('store_product');
+    Route::get('/edit-product/{product}', [ProductController::class, 'edit'])->name('edit_product');
+    Route::patch('/update-product/{product}', [ProductController::class, 'update'])->name('update_product');
+    Route::delete('/delete-product/{product}', [ProductController::class, 'delete'])->name('delete_product');
 
+
+    Route::post('/order-confirm-payment/{order}',[OrderController::class, 'confirm_payment'])->name('confirm_payment');
+});
+
+Route::middleware(['auth'])->group(function(){
+    //product
+    Route::get('/show-product/{product}', [ProductController::class, 'show'])->name('show_product');
+
+    //cart
+    Route::post('/store-cart/{product}', [CartController::class, 'add_to_cart'])->name('store_cart');
+    Route::get('/show-cart', [CartController::class, 'show'])->name('show_cart');
+    Route::patch('/update-cart/{cart}', [CartController::class, 'update'])->name('update_cart');
+    Route::delete('/delete-cart/{cart}', [CartController::class,'delete'])->name('delete_cart');
+
+    //orders
+    Route::post('/checkout-order',[OrderController::class, 'checkout'])->name('checkout');
+    Route::get('/index-order',[OrderController::class,'index'])->name('index_order');
+    Route::get('/show-order/{order}',[OrderController::class, 'show_detail_order'])->name('show_detail_order');
+    Route::post('/order-payment/{order}',[OrderController::class, 'receipt'])->name('submit_payment_receipt');
+
+
+    //user information
+    Route::get('/profile',[ProfileController::class,'show_profile'])->name('show_profile');
+    Route::post('/edit-profile',[ProfileController::class, 'edit_profile'])->name('edit_profile');
+});
 
